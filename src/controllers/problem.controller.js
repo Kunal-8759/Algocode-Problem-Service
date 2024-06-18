b7a4ad76dad7ad8a35faf6db90d6d3142fa7126a
Kunal Kumar <kumarkunal8759@gmail.com>
2024-06-18 13:05:08 +0530

    fix:migrated from the numeric status code to http status code

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,29 +1,32 @@
+const {StatusCodes}=require('http-status-codes');
+
+
 function problemController(req,res){
     return res.json({message:'i am ok'});
 }
 
 function addProblem(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"not implemented"
     });
 }
 function getProblem(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"not implemented"
     });
 }
 function deleteProblem(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"not implemented"
     });
 }
 function getProblems(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"not implemented"
     });
 }
 function updateProblem(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"not implemented"
     });
 }
